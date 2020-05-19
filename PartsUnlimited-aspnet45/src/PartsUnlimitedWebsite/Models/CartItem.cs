ae487f6e787509421b9491aec398b0a64c970081
Megha Singh <megha.e.singh@accenture.com>
2020-05-19 11:37:55 +0530

    second commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My first commit
+        //My second commit
         [Key]
         public int CartItemId { get; set; }
 
